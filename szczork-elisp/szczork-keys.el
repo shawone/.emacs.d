9ff5d53ccbe774f7a36e96bf9cbed0e7f9d1b833
shawone <shawone@enicom.org>
2013-12-16 11:54:56 +0100

    create sub-directories

--- szczork-elisp/szczork-keys.el
+++ szczork-elisp/szczork-keys.el
@@ -5,3 +5,5 @@
 (global-set-key (kbd "C-x c") 'whitespace-cleanup)
 (global-set-key (kbd "C-c c") 'comment-region)
 (global-set-key (kbd "C-c u") 'uncomment-region)
+
+(provide 'szczork-keys)
---
+++ szczork-elisp/szczork.el
@@ -0,0 +1,102 @@
+(set-language-environment "UTF-8")
+
+(add-to-list 'load-path "~/.emacs.d/szczork-elisp/")
+(column-number-mode t)
+
+(setq inhibit-startup-screen t          ;do not display a startup screen
+      autopair-autowrap t               ;wrap the region with the paired characters
+      mouse-yank-at-point t             ;paste at cursor position
+      scroll-preserve-screen-position t ;restore cursor after PgUp/PgDown
+
+      ;; Sentences end with one space when M-q `fill-paragraph' is called
+      sentence-end-double-space nil
+
+      compilation-auto-jump-to-first-error t
+      compilation-scroll-output t
+
+      ;; C-k kills whole line and newline if at beginning of line
+      kill-whole-line t
+
+      ;; Don't want to answer yes everytime
+      save-abbrevs 'silently
+
+      ;; Autosave each change)
+      bookmark-save-flag 1
+
+      ;; Use Window system clipboard
+      x-select-enable-clipboard t
+      yank-pop-change-selection t
+
+      ;; Case-insensitive `find-file'
+      read-file-name-completion-ignore-case t
+      read-buffer-completion-ignore-case t)
+
+;;remove tab
+(setq-default indent-tabs-mode nil)
+
+(setq-default save-place t)
+(require 'saveplace)
+(setq save-place-file "~/.emacs.d/saved-places")
+
+(setq completion-ignored-extensions '(".o" ".a" ".so"
+                                      ".elc"
+                                      ".class" ".dll"
+                                      "~"
+                                      ".git/"
+                                      ".gitignore"
+                                      ".hg/"
+                                      ".hgignore"
+                                      ".svn/"))
+
+;toolbar is visible by default on X emacs so disable it
+(if window-system
+    (tool-bar-mode -1))
+(blink-cursor-mode -1)
+(if window-system
+          (scroll-bar-mode -1))
+(column-number-mode 1)
+(winner-mode 1)
+;; Replace selection
+(delete-selection-mode 1)
+
+;; Highlight current line
+(if (boundp 'global-hl-line-sticky-flag) ;introduced in emacs 24.1
+    (setq global-hl-line-sticky-flag t))
+(global-hl-line-mode 1)
+
+;; Whenever an external process changes a file underneath emacs, and there
+;; was no unsaved changes in the corresponding buffer, just revert its
+;; content to reflect what's on-disk.
+;; source: https://github.com/dimitri/emacs-kicker/blob/master/init.el
+(global-auto-revert-mode 1)
+
+;; Show the matching parenthesis
+(require 'paren)
+(show-paren-mode 1)
+
+ ;; Go to the next page automatically in Doc-View
+(setq doc-view-continuous t)
+
+; Automatically 'chmod' scripts as they are saved
+(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
+
+;; Narrowing is convenient
+(put 'widen 'disabled nil)
+(put 'narrow-to-defun 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
+(put 'org-narrow-to-subtree 'disabled nil)
+(put 'dired-find-alternate-file 'disabled nil)
+
+(defadvice narrow-to-region (after narrow-to-region-unmark
+activate compile)
+  "Disable selection after `narrow-to-region'"
+  (deactivate-mark))
+;; KeysBinding
+(require 'szczork-keys)
+
+;; Load path theme & load theme
+(setq my-theme-dir "~/.emacs.d/szczork-themes/")
+(add-to-list 'load-path my-theme-dir)
+(setq custom-theme-directory my-theme-dir)
+(load-theme 'szczork t)
