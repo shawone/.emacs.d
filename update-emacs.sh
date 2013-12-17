a5932056beb24fd8b152bd0de8a7daedb5cbf9ec
shawone <shawone@enicom.org>
2013-12-17 13:18:37 +0000

    init script

--- update-emacs.sh
+++ update-emacs.sh
@@ -2,7 +2,7 @@
 ## Ubuntu 12.04 LTS
 ##
 
-apt-get install python-software-properties -y
-sudo add-apt-repository ppa:cassou/emacs
-sudo apt-get update
-sudo apt-get install emacs24
+sudo apt-get install python-software-properties -y
+sudo add-apt-repository ppa:cassou/emacs -y
+sudo apt-get update -y
+sudo apt-get install emacs24 -y
