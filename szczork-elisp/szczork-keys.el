1828138bab1cb9c4f110f775e5e28b670c6d9558
Nicolas Chevalier <nicolas.chevalier@epitech.eu>
2014-04-01 15:05:03 +0200

    Update szczork-keys.el

--- szczork-elisp/szczork-keys.el
+++ szczork-elisp/szczork-keys.el
@@ -1,8 +1,14 @@
 ;; KeysBindings
 
-(define-key mode-specific-map (kbd "C-b") 'shell)
+;; (ctrl+c)(s)
+(define-set-key (kbd "C-c s") 'shell)
 
+;; (ctl+x)(c) for delete space char
 (global-set-key (kbd "C-x c") 'whitespace-cleanup)
+
+;;step1-> (ctrl+space)(arrow) for selection
+;;step2-> (ctrl+c)(c) for comment
+;; or  -> (ctrl+c)(u) for uncomment
 (global-set-key (kbd "C-c c") 'comment-region)
 (global-set-key (kbd "C-c u") 'uncomment-region)
 
